2c0c140d1cc807c0cb474da3a029338179483aa8
Felipe Tadeu <souzalima@gmail.com>
2023-04-09 16:43:33 -0300

    corrected 08_classes_structs

--- d00/08_class_vs_struct/Sample1.class.hpp
+++ d00/08_class_vs_struct/Sample1.class.hpp
@@ -6,7 +6,7 @@
 /*   By: ftadeu-d <ftadeu-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 14:08:28 by ftadeu-d          #+#    #+#             */
-/*   Updated: 2023/04/09 14:15:25 by ftadeu-d         ###   ########.fr       */
+/*   Updated: 2023/04/09 16:42:23 by ftadeu-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ struct Sample1 {
     int     foo;
 
     Sample1(void);
+    void bar(void);
     ~Sample1(void);
 
-    void    bar(void) const;
 
 };
 
--- d00/08_class_vs_struct/Sample2.class.hpp
+++ d00/08_class_vs_struct/Sample2.class.hpp
@@ -6,7 +6,7 @@
 /*   By: ftadeu-d <ftadeu-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 14:10:57 by ftadeu-d          #+#    #+#             */
-/*   Updated: 2023/04/09 14:15:22 by ftadeu-d         ###   ########.fr       */
+/*   Updated: 2023/04/09 16:42:47 by ftadeu-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@
 
 class Sample2 {
 
+public:
     int     foo;
 
     Sample2(void);
+    void bar(void);
     ~Sample2(void);
-
-    void    bar(void) const;
 
 };
 
